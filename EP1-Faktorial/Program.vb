f7b0bcc705d8a26b820eb748122e9df3bacc6e19
Karel Pírek <Pirek.Karel@stud.vassboskovice.cz>
2024-05-30 09:52:58 +0200

    Nelze zadat nic jiného než celé číslo

--- EP1-Faktorial/Program.vb
+++ EP1-Faktorial/Program.vb
@@ -5,14 +5,15 @@ Module Program
         Dim cislo As Integer
         Dim faktorial As Integer = 1
 
-        Console.Write("Zadej èíslo: ")
-        cislo = Console.ReadLine
-
-        For i = 1 To cislo
-            faktorial = faktorial * i
-        Next i
-
-        Console.WriteLine($"Faktoriá1 èísla {cislo} je {faktorial}")
+        Console.Write("Zadej celé èíslo: ")
+        If Integer.TryParse(Console.ReadLine, cislo) Then
+            For i = 1 To cislo
+                faktorial = faktorial * i
+            Next i
+            Console.WriteLine($"Faktoriá1 èísla {cislo} je {faktorial}")
+        Else
+            Console.WriteLine("Zadejte celé èíslo.")
+        End If
 
     End Sub
 End Module
