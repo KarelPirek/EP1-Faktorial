1544dc92722d89258c00695d9231682ef79632f0
Karel Pírek <Pirek.Karel@stud.vassboskovice.cz>
2024-05-30 09:45:40 +0200

    Výpočet faktoriálu

--- EP1-Faktorial/Program.vb
+++ EP1-Faktorial/Program.vb
@@ -1,7 +1,18 @@
 Imports System
 
 Module Program
-  Sub Main(args As String())
-    Console.WriteLine("Hello World!")
-  End Sub
+    Sub Main(args As String())
+        Dim cislo As Integer
+        Dim faktorial As Integer = 1
+
+        Console.Write("Zadej èíslo: ")
+        cislo = Console.ReadLine
+
+        For i = 1 To cislo
+            faktorial = faktorial * i
+        Next i
+
+        Console.WriteLine($"Faktoriá1 èísla {cislo} je {faktorial}")
+
+    End Sub
 End Module
