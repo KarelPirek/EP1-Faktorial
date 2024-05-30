be74d8001d53ba2ec9c4a20f5a3a7454f6509383
Karel Pírek <Pirek.Karel@stud.vassboskovice.cz>
2024-05-30 10:11:46 +0200

    Nelze zadat větší číslo než 12 (číslo 12 stále nevyvolá výjimku)

--- EP1-Faktorial/Program.vb
+++ EP1-Faktorial/Program.vb
@@ -7,10 +7,14 @@ Module Program
 
         Console.Write("Zadej celé èíslo: ")
         If Integer.TryParse(Console.ReadLine, cislo) Then
-            For i = 1 To cislo
-                faktorial = faktorial * i
-            Next i
-            Console.WriteLine($"Faktoriá1 èísla {cislo} je {faktorial}")
+            If cislo < 13 Then
+                For i = 1 To cislo
+                    faktorial = faktorial * i
+                Next i
+                Console.WriteLine($"Faktoriá1 èísla {cislo} je {faktorial}")
+            Else
+                Console.WriteLine("Faktoriál èísel 13 a více je vìtší, než nejvìtší èíslo, které spadá do celých èísel.")
+            End If
         Else
             Console.WriteLine("Zadejte celé èíslo.")
         End If
