ae96362c0c30eb0b8e0374746376b1b521b3945e
Karel Pírek <Pirek.Karel@stud.vassboskovice.cz>
2024-05-30 10:15:58 +0200

    Nelze zadávat záporné hodnoty

--- EP1-Faktorial/Program.vb
+++ EP1-Faktorial/Program.vb
@@ -8,12 +8,16 @@ Module Program
         Console.Write("Zadej celé èíslo: ")
         If Integer.TryParse(Console.ReadLine, cislo) Then
             If cislo < 13 Then
-                For i = 1 To cislo
-                    faktorial = faktorial * i
-                Next i
-                Console.WriteLine($"Faktoriá1 èísla {cislo} je {faktorial}")
+                If cislo > -1 Then
+                    For i = 1 To cislo
+                        faktorial = faktorial * i
+                    Next i
+                    Console.WriteLine($"Faktoriá1 èísla {cislo} je {faktorial}")
+                Else
+                    Console.WriteLine("Faktoriál záporného èísla nelze vypoèítat.")
+                End If
             Else
-                Console.WriteLine("Faktoriál èísel 13 a více je vìtší, než nejvìtší èíslo, které spadá do celých èísel.")
+                    Console.WriteLine("Faktoriál èísel 13 a více je vìtší, než nejvìtší èíslo, které spadá do celých èísel.")
             End If
         Else
             Console.WriteLine("Zadejte celé èíslo.")
