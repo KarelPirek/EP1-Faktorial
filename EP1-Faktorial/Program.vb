9309b15706b96a4ce3f7598265e30cf17dc07092
Karel Pírek <Pirek.Karel@stud.vassboskovice.cz>
2024-06-05 10:31:00 +0200

    Oprava chyb

--- EP1-Faktorial/Program.vb
+++ EP1-Faktorial/Program.vb
@@ -2,36 +2,44 @@ Imports System
 
 Module Program
     Sub Main(args As String())
-
-        Console.Write("Zadej celé èíslo: ")
         Dim Vysledek As Integer
-        Vysledek = FaktorialVypocet()
-        Console.WriteLine($"Faktoriá1 zadaného èísla je {Vysledek}")
-    End Sub
-    Function FaktorialVypocet(cislo As Integer)
-
-
+        Dim vstup As Integer
         Do
-            Dim faktorial As Integer = 1
-
-            If Integer.TryParse(Console.ReadLine, cislo) Then
-                If cislo < 13 Then
-                    If cislo > -1 Then
-                        For i = 1 To cislo
-                            faktorial = faktorial * i
-                        Next i
-
+            Console.Write("Zadej celé èíslo: ")
+            If Integer.TryParse(Console.ReadLine, vstup) Then
+                If vstup < 13 Then
+                    If vstup > -1 Then
+                        Vysledek = FaktorialVypocet(vstup)
                     Else
                         Console.WriteLine("Faktoriál záporného èísla nelze vypoèítat.")
                     End If
                 Else
                     Console.WriteLine("Faktoriál èísel 13 a více je vìtší, než nejvìtší èíslo, které spadá do celých èísel.")
                 End If
-            Else
-                Console.WriteLine("Zadejte celé èíslo.")
+
+                Console.WriteLine($"Faktoriá1 zadaného èísla je {Vysledek}")
+                    Else
+                        Console.WriteLine("Zadejte celé èíslo.")
             End If
         Loop
-        Return 0
+    End Sub
+    Function FaktorialVypocet(cislo As Integer)
+
+
+
+        Dim faktorial As Integer = 1
+
+
+
+        For i = 1 To cislo
+            faktorial = faktorial * i
+        Next
+
+
+
+
+
+        Return faktorial
     End Function
 
 End Module
